26b48d89d58094de71608fd51190cdd1fa3c6cfa
pothi <shanmugapothi@gmail.com>
2026-02-21 15:54:34 +0530

    initial project setup with profesional folder structure and routing skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',loadComponent:()=>import('./features/auth/login/login.component').then(m=>m.LoginComponent)},
+    {path:'',loadComponent:()=>import('./layout/main-layout/main-layout.component').then(m=>m.MainLayoutComponent),
+    children:[
+        {path:'dashboard',loadComponent:()=>import('./features/dashboard/dashboard.component').then(m=>m.DashboardComponent)},
+        {path:'employees',loadComponent:()=>import('./features/employees/employees.component').then(m=>m.EmployeesComponent)},
+        {path:'',loadComponent:()=>import('./features/leave/leave.component').then(m=>m.LeaveComponent)},
+        {path:'',loadComponent:()=>import('./features/profile/profile.component').then(m=>m.ProfileComponent)}
+
+    ]
+},
+];
